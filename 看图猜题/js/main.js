a0a3295de905afeab564b7538bded6fa43f2b783
daboluoxigua <37130436+daboluoxigua@users.noreply.github.com>
2018-08-27 12:52:04 +0800

    Add files via upload

---
+++ 看图猜题/js/main.js
@@ -0,0 +1,236 @@
+var asList = [{
+	"path": ["images/img/img1/1.jpg","images/img/img1/2.jpg","images/img/img1/3.jpg","images/img/img1/4.jpg"],
+	"answer": "中美贸易战",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img2/1.jpg","images/img/img2/2.jpg","images/img/img2/3.jpg","images/img/img2/4.jpg"],
+	"answer": "区块链",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img3/1.jpg","images/img/img3/2.jpg","images/img/img3/3.jpg","images/img/img3/4.jpg"],
+	"answer": "两会召开",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img4/1.jpg","images/img/img4/2.jpg"],
+	"answer": "中国芯事件",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img5/1.jpg","images/img/img5/2.jpg","images/img/img5/3.jpg"],
+	"answer": "合规备案",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img6/1.jpg","images/img/img6/2.jpg","images/img/img6/3.jpg"],
+	"answer": "美联储利率决议",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img7/1.jpg","images/img/img7/2.jpg","images/img/img7/3.jpg"],
+	"answer": "房价下跌",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img8/1.jpg","images/img/img8/2.jpg","images/img/img8/3.jpg","images/img/img8/4.jpg"],
+	"answer": "世界经济论坛",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img9/1.jpg","images/img/img9/2.jpg","images/img/img9/3.jpg"],
+	"answer": "一带一路",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img10/1.jpg","images/img/img10/2.jpg","images/img/img10/3.jpg"],
+	"answer": "港珠澳大桥",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img11/1.jpg","images/img/img11/2.jpg","images/img/img11/3.jpg"],
+	"answer": "博鳌论坛",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img12/1.jpg"],
+	"answer": "上合组织青岛峰会",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img13/1.jpg","images/img/img13/2.jpg","images/img/img13/3.jpg"],
+	"answer": "南海阅兵",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img14/1.jpg","images/img/img14/2.jpg"],
+	"answer": "俄罗斯总统大选",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img15/1.jpg","images/img/img15/2.jpg","images/img/img15/3.jpg"],
+	"answer": "改革开放四十周年",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img16/1.jpg","images/img/img16/2.jpg","images/img/img16/3.jpg"],
+	"answer": "广交会",
+	"hint":"上半年发生的大事件"
+}, {
+	"path": ["images/img/img17/1.jpg","images/img/img17/2.jpg","images/img/img17/3.jpg"],
+	"answer": "消费者权益日",
+	"hint":"上半年发生的大事件"
+}];
+
+
+//父亲节/////////////////////////////////////////////////////////////////////
+var father = [ {
+	"path": ["images/img/img18/1.jpg","images/img/img18/2.jpg"],
+	"answer": "背影",
+	"hint":"猜一文学作品"
+}, {
+	"path": ["images/img/img19/1.jpg","images/img/img19/2.jpg"],
+	"answer": "父亲",
+	"hint":"猜一歌名"
+}, {
+	"path": ["images/img/img20/1.jpg","images/img/img20/2.jpg"],
+	"answer": "替父从军",
+	"hint":"猜一故事"
+}]
+
+//端午节/////////////////////////////////////////////////////////////////////
+var dragon = [{
+	"path": ["images/img/img21/1.jpg","images/img/img21/2.jpg","images/img/img21/3.jpg"],
+	"answer": "雄黄酒",
+	"hint":"猜一端午节食品"
+}, {
+	"path": ["images/img/img22/1.jpg"],
+	"answer": "咸粽子",
+	"hint":"猜猜粽子的口味"
+}, {
+	"path": ["images/img/img23/1.jpg"],
+	"answer": "艾叶",
+	"hint":"端午节，图中物品里会装什么？"
+}]
+
+
+asList=asList.concat(father)//加入父亲节题目
+asList=asList.concat(dragon)//加入端午节题目
+
+var library = "体、组、织、化、盟、争、联、球、全、进、政、商、口、张、倒、拉、维、善、张、学、加、发、生、满、知、的、方、地、电、外、存、字、随、虽、着、集、圣、身、提、万、建、盘、潘、盼、从、次、好、标、歌、开、圈、富、浩、敏、鸣、道、丽、这、包、振、送、了、乐、领、李、民、迷、做、此、司、离、闸、真、及、聪、文、遗、现、入、来、码、麻、债、摘、挚、艾、到、领、兑、英、余、过、放、及、级、找、条、先、数、别、作、融、术、技、金、局、格、核、革、化、新、未、算、解、心、命、东、北、西、程、公、工、世、任、目、化、典、通、元、荣、起、户、路、入、拆、需、货、每、何、加、归、常、编、彼、才、穴、对、说、亿、个、其、候、家、卡、想、堪、垃、推、辣、找、果、改、咖、噶、写、哈、哦、景、赵、浩、贵、游、双、源、聪、宏、资、天、市、程、项、哲、议、涨、退、审、深、成、最、账、升、力、掌、天、握、生、湾、概、红、机、时、品、阅、视、设、微、布、长、温、台、接、注、安、同、洽、无、谈、省、王、董、里、外、件、兴、新、信、鹏、志、罗、消、扁、总、夏、刚、购、广、辜、良、测、社、永、曾、比、爱、闵、钟、想、系、都、协、吖、蜡、超、董、天、在、可、雷、家、千、了、率、及、这、琪、情、偶、欧、参、与、于、雨、投、透、海、图、拍、照、信、冉、后、部、梯、删、载、问、答、恩、匹、缪、养、诚、已、从、喜、社、平、和、测、暖、潮、全、文、图、兰、示、利、理、开、代、库、民、汇、已、哈、商、忍、啊、豪、目、至、愈、就、早、粽、子、噢、破、供、计、奇、嗨、旺、铁、胡、慧、规、智、猜、注、将、木、朱、果、把、不、司、法、庆、雪、马、亮、吴、兰、射、伐、监、官、管、速、读、那、邋、淑、廖、平、倪、红、纷、老、房、价、琳、临、咋、请、喝、土、途、徐、续、继、需、球、春、季、专、职、光、洋、阳、氧、雅、压、汽、钱、币、戴、袋、笔、水、税、瓜、说、置、概、邀、要、务、木、呗、花、付、贝、氧、息、耐、钮、算、话、练、桂、定、回、历、照、偏、咯、宏、锡、百、登、摄、产、扭、邓、扎、撼、明、林、重、黑、降、莫、南、统、走、跑";
+
+
+var answ = document.getElementById("answerBar");
+var wre = document.getElementById("write");
+var ask;
+var flg = false;
+
+var i = 0;
+var s_index = 0;
+
+function init() {
+	i = 0;
+
+	ask = asList.sort(function() {
+		return Math.random() - Math.random();
+	});
+	
+	console.table(ask)
+}
+
+function askLoad(i) {
+	s_index = 0;
+	
+
+	//图片乱序
+	var imgList = ask[i].path.sort(function() {
+		return Math.random() - Math.random();
+	});
+
+	
+	//答案乱序
+	var spanList = library.split("、").sort(function() {
+		return Math.random() - Math.random();
+	});
+	
+	
+	spanList.length=library.length-(library.length - 21)-ask[i].answer.length;//设置动态数组长度
+	spanList=spanList.concat(ask[i].answer.split("")).sort(function() {
+		return Math.random() - Math.random();
+	});
+	
+	
+	//赋值到答题选项
+	var answ_str = "";
+	for(var x = 0; x < spanList.length; x++) {
+		answ_str += '<span class=' + "s" + x + '>' + spanList[x] + '</span>'
+	}
+	$(".answerBar").html(answ_str)
+
+	$(".img").attr('src', imgList[0]) //设置图片
+	$(".hint span").html(ask[i].hint) //设置提示
+	
+	
+	//设置答案
+	var ask_str = "";
+	for(var a = 0; a < ask[i].answer.length; a++) {
+		ask_str += '<span></span>'
+	}
+	$(".write").html(ask_str)
+	
+
+	//选择答案
+	$(".answerBar span").click(function() {
+		var athis = $(this);
+
+		$(".write span").each(function(index) {
+			
+			
+			if($(this).html() == "") { // 在填空为空时插入答案
+				$(this).html(athis.html()).attr("class", athis.attr("class"));
+				athis.html("");
+				s_index = index;
+
+				if(index == $(".write span").length - 1) { //答至最后一空的时候串联所答内容赋值
+					var wrong_str = "";
+					for(var z = 0; z < $(".write span").length; z++) {
+						wrong_str += $(".write span").eq(z).html()
+					}
+
+					//判断是否通过
+					if(contrast(wrong_str, ask[i].answer)) {
+						setTimeout(function(){
+							popup(".win");
+						},500)
+					} else {
+						setTimeout(function(){
+							popup(".lost");
+						},500)
+					}
+
+				}
+
+				return false;
+			}
+
+		});
+
+	});
+
+	$(".backspace").unbind("click").click(function() {
+		if(s_index >=0) {
+			$(".answerBar span").each(function() {
+				
+				if($(this).attr("class") == $(".write span").eq(s_index).attr("class")) {					
+					$(this).html($(".write span").eq(s_index).html());
+					$(".write span").eq(s_index).html("").removeClass();
+				}
+			})
+			
+			if(s_index==0){
+				s_index=0;
+				console.log("已经清零")
+			}else{
+				s_index--;
+			}
+			
+		}
+
+	});
+
+	console.table(ask[i])
+}
+
+function contrast(wrg, ask) {
+	if(wrg === ask) {
+		return true;
+	} else {
+		return false;
+	}
+}
